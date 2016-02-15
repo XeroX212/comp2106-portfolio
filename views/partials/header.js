791bb0a331865f460d3348abc19bff16b3793611
Xerox212 <arunpreet37@gmail.com>
2016-02-15 15:19:01 -0500

    init

---
+++ views/partials/header.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>VC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    
+    <!-- Link To Stylesheet, Custom Stylesheet & Icons --> 
+    
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="/stylesheets/materialize.css" media="screen,projection" />
+      <link href="/stylesheets/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      
+    <!-- javaScript -->
+    
+    <script src="/javascripts/jquery-2.2.0.min.js"></script>
+    <script src="/javascripts/materialize.js"></script>
+  </head>
+<body>
+<div class="navbar-fixed">
+ <nav>
+    <div class="nav-wrapper">
+      <a href="/" class="brand-logo center"><i class="material-icons">desktop_mac</i></a>
+       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a class="waves-effect" href="/about" title=about>About Me</a></li>
+        <li><a class="waves-effect" href="/portfolio" title="portfolio">Portfolio</a></li>
+        <li><a class="waves-effect" href="/contact" title="contact">Contact</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="waves-effect" href="/" title="Home">About Me</a></li>
+        <li><a class="waves-effect" href="/portfolio" title="portfolio">Portfolio</a></li>
+        <li><a class="waves-effect" href="/contact" title="contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
